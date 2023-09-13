4c9426150cbbc397a13e1153a0216bcd3119683d
ledavto <ser_ega@ukr.net>
2023-09-13 22:56:06 +0300

    edit video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulEl = document.querySelector(".gallery");
+
+const imgEl = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" data-caption="${description}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+ulEl.insertAdjacentHTML("beforeend", imgEl);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
