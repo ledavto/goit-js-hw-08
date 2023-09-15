d338066de802b0bb22fbeedcc1c0a03879f7bfcd
ledavto <ser_ega@ukr.net>
2023-09-15 16:48:13 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,50 +1,59 @@
-//import Player from '@vimeo/player';
+import Player from '@vimeo/player';
 
 //var throttle = require('lodash.throttle');
 
 import throttle from 'lodash';
 
-console.log(_.throttle);
+//console.log(throttle);
 
-// const iframe = document.querySelector('iframe');
+const iframe = document.querySelector('iframe');
 
-// const player = new Player(iframe);
+const player = new Player(iframe);
 
-// const timeVideo = localStorage.getItem('videoplayer-current-time');
+const timeVideo = localStorage.getItem('videoplayer-current-time');
 
-// player
-//   .setCurrentTime(timeVideo)
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
+player
+  .setCurrentTime(timeVideo)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
-// var nowTime = _.throttle(() => {
+// var nowTime = throttle(() => {
 //   console.log(456);
 // }, 1000);
 
-// player.on('play', nowTime);
+function nowTime() {
+  player
+    .getCurrentTime()
+    .then(function (seconds) {
+      //seconds = the current playback position
+      console.log(seconds);
+      localStorage.setItem('videoplayer-current-time', seconds);
+    })
+    .catch(function (error) {
+      // an error occurred
+    });
 
-// function nowTime() {
-//   player
-//     .getCurrentTime()
-//     .then(function (seconds) {
-//       //seconds = the current playback position
-//       console.log(seconds);
-//     })
-//     .catch(function (error) {
-//       // an error occurred
-//     });
+  // console.log(nowTime);
+}
 
-//   console.log(nowTime);
-// }
+//player.on('timeupdate', throttle(nowTime, 1000));
+player.on('timeupdate', nowTime);
+
+// player.on(
+//   'play',
+//   throttle(() => {
+//     console.log(456);
+//   }, 1000)
+// );
